ea5bb9fe80f41f69abaed4586606248b5c7fbe87
KimJaeYoung1028 <124559290+KimJaeYoung1028@users.noreply.github.com>
2023-04-27 15:22:59 +0900

    Add files via upload

---
+++ USER.sql
@@ -0,0 +1,62 @@
+create table USER
+(
+    UserID      int auto_increment
+        primary key,
+    UserName    varchar(36) not null,
+    UserImage   blob        null,
+    PhoneNumber varchar(11) not null,
+    Created     timestamp   not null,
+    Updated     timestamp   null,
+    Statuss     int         null
+);
+
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (1, 'association5872415609', null, '(010) 7641-', '2022-01-01 15:17:00', '2022-04-11 15:17:00', null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (2, 'prestigious2549505051', null, '(010) 5189-', '2022-01-07 10:39:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (3, 'gathering2866265292', null, '(010) 7386-', '2022-01-12 18:55:00', '2022-02-04 18:55:00', 1);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (4, 'humanresources8657674543', null, '(010) 4545-', '2022-01-18 20:46:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (5, 'engagement7073580587', null, '(010) 9827-', '2022-01-28 10:59:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (6, 'alternate5437737012', null, '(010) 9979-', '2022-02-04 18:26:00', '2022-02-16 18:26:00', null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (7, 'advanced9326362117', null, '(010) 1054-', '2022-02-17 05:36:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (8, 'notice1345066160', null, '(010) 5232-', '2022-02-21 04:40:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (9, 'flashing4315247616', null, '(010) 9895-', '2022-03-03 07:29:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (10, 'vibrate69133426', null, '(010) 6021-', '2022-03-19 18:33:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (11, 'endtable7225952642', null, '(010) 9293-', '2022-04-01 04:38:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (12, 'slightly3464273669', null, '(010) 8391-', '2022-04-13 01:01:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (13, 'recognize5799667321', null, '(010) 7708-', '2022-04-18 00:02:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (14, 'hesitant9271445651', null, '(010) 9863-', '2022-04-21 11:51:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (15, 'haveover4066644443', null, '(010) 3322-', '2022-04-23 00:22:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (16, 'relief6502143717', null, '(010) 7163-', '2022-04-23 07:20:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (17, 'flood7759381373', null, '(010) 2137-', '2022-04-24 08:09:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (18, 'occasionally2292050279', null, '(010) 7063-', '2022-05-09 06:44:00', '2022-05-16 06:44:00', null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (19, 'catchupon5110169039', null, '(010) 6488-', '2022-05-11 20:58:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (20, 'exceptional8470062632', null, '(010) 6450-', '2022-05-26 19:11:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (21, 'composition7732064424', null, '(010) 6471-', '2022-05-27 03:06:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (22, 'tense8162936072', null, '(010) 7100-', '2022-05-28 06:30:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (23, 'patiently331905328', null, '(010) 6279-', '2022-06-01 09:18:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (24, 'forthcoming9143265565', null, '(010) 6658-', '2022-06-22 01:33:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (25, 'ruddy3634719460', null, '(010) 4349-', '2022-07-09 19:28:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (26, 'blowaway1046692593', null, '(010) 3881-', '2022-08-08 00:43:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (27, 'masterpiece9382450861', null, '(010) 8851-', '2022-08-12 17:49:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (28, 'self-pity2582810742', null, '(010) 5564-', '2022-08-18 11:20:00', '2022-09-30 11:20:00', 1);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (29, 'descend1576836544', null, '(010) 4903-', '2022-08-19 05:26:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (30, 'bloom9174414734', null, '(010) 5535-', '2022-08-27 18:29:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (31, 'manor3397480559', null, '(010) 8348-', '2022-09-06 02:32:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (32, 'sticky-fingered8620510519', null, '(010) 6307-', '2022-09-18 08:47:00', '2022-12-05 08:47:00', null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (33, 'loop2938301405', null, '(010) 1259-', '2022-09-19 22:07:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (34, 'lengthen3840750056', null, '(010) 9182-', '2022-09-23 12:44:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (35, 'edge9436170486', null, '(010) 5018-', '2022-10-02 20:44:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (36, 'wrap972246931', null, '(010) 7828-', '2022-10-05 23:11:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (37, 'disturbance9205431889', null, '(010) 5776-', '2022-10-15 20:36:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (38, 'stripe6458073762', null, '(010) 9208-', '2022-10-21 14:00:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (39, 'deckchair363812532', null, '(010) 9004-', '2022-11-12 21:08:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (40, 'float6177078589', null, '(010) 6764-', '2022-11-15 13:53:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (41, 'petal8138610658', null, '(010) 6223-', '2022-11-24 11:07:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (42, 'editor178871609', null, '(010) 4396-', '2022-11-26 01:52:00', '2023-03-04 01:52:00', 1);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (43, 'admission9443937984', null, '(010) 2757-', '2022-12-01 02:00:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (44, 'reject4813921323', null, '(010) 3933-', '2022-12-28 02:58:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (45, 'overwhelmed3836272327', null, '(010) 9591-', '2023-01-17 08:22:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (46, 'frontoffice2804925914', null, '(010) 4945-', '2023-01-21 09:20:00', '2023-01-31 09:20:00', 1);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (47, 'pound9883795051', null, '(010) 5196-', '2023-02-06 10:43:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (48, 'shortofbreath7322756011', null, '(010) 1070-', '2023-02-07 11:01:00', null, null);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (49, 'dial8905913319', null, '(010) 9409-', '2023-02-07 22:38:00', '2023-03-11 22:38:00', 1);
+INSERT INTO dbtest.USER (UserID, UserName, UserImage, PhoneNumber, Created, Updated, Statuss) VALUES (50, 'attract8210767613', null, '(010) 9993-', '2023-02-10 02:13:00', null, null);
